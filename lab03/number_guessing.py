f4800a1a723c564781b71f9f8147c6399a824122
TekihcaN <nachiketdani4587@gmail.com>
2019-09-19 21:32:12 -0700

    Corrected extremely hot,guess1 to guess

--- lab03/number_guessing.py
+++ lab03/number_guessing.py
@@ -4,12 +4,11 @@ def main():
 
     ##### Generate random number between 1 and 50 for the game
     rand_num = random.randint(1,50)
-    # print (rand_num) ## Print random number generated for reference purposes while validating code
     
     ##### Initation of game and guess counter
     print ("Welcome to the Guessing Game!")
-    guess1 = int((input ("I picked a number between 1 and 50. Try and guess!" "\n")))
-    print("You guessed", guess1)
+    guess = int((input ("I picked a number between 1 and 50. Try and guess!" "\n")))
+    print("You guessed", guess)
     guess_count = 1
     
     ##### Initiate variable delta for calculating approach to answer
@@ -17,30 +16,30 @@ def main():
 
     ##### Determine clue to be provided and ask for next guess while incrementing guess count by 1
     while delta > 0:
-        delta = abs(guess1-rand_num)
+        delta = abs(guess-rand_num)
         if (delta == 1):
-            guess1 = int (input ("Your guess is scalding hot \n"))
+            guess = int(input ("Your guess is scalding hot \n"))
             guess_count += 1
         elif (delta == 2): 
-            guess1 = int (input ("Your guess is extremely hot \n"))
+            guess = int(input ("Your guess is extremely warm \n"))
             guess_count += 1
         elif (delta == 3):
-            guess1 = int (input ("Your guess is very warm \n"))
+            guess = int(input ("Your guess is very warm \n"))
             guess_count += 1
-        elif (delta > 3 and abs(guess1-rand_num) <= 5):
-            guess1 = int (input ("Your guess is warm \n"))
+        elif (delta > 3 and abs(guess-rand_num) <= 5):
+            guess = int(input ("Your guess is warm \n"))
             guess_count += 1
         elif (delta > 5 and delta <= 8):
-            guess1 = int (input ("Your guess is cold \n"))
+            guess = int(input ("Your guess is cold \n"))
             guess_count += 1
         elif (delta > 8 and delta <= 13):
-            guess1 = int (input ("Your guess is very cold \n"))
+            guess = int(input ("Your guess is very cold \n"))
             guess_count += 1
         elif (delta > 13 and delta <= 20):
-            guess1 = int (input ("Your guess is extremely cold \n"))
+            guess = int(input ("Your guess is extremely cold \n"))
             guess_count += 1
         elif (delta > 20):
-            guess1 = int (input ("Your guess is icy freezing miserably cold \n"))
+            guess = int(input ("Your guess is icy freezing miserably cold \n"))
             guess_count += 1
 
     print ("Congratulations. You figured it out in", guess_count, "tries")
