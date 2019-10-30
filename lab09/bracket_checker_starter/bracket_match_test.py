c8806ce2b63773b491449488e47f716276000174
TekihcaN <nachiketdani4587@gmail.com>
2019-10-30 11:24:16 -0700

    Hw08: Completed unigram logic; Lab09: Completed ex01

---
+++ lab09/bracket_checker_starter/bracket_match_test.py
@@ -0,0 +1,13 @@
+from bracket_match import BracketMatch
+
+
+def test_brackets_match():
+    """Test brackets_match method"""
+    # Include the following cases in your test:
+    # "()" should succeed
+    # "a(a[a])a({a})" should succeed
+    # "(" should not succeed
+    # "(}" should not succeed
+    # "a(a(a)a(a)" should not succeed
+    # "aa(a))a(a)" should not succeed
+    assert False'
